27d92187f5aa112e439774274f640a0c5f54bf40
Alex <collo@DESKTOP-0MTRO7E>
2022-11-08 19:47:47 +0100

    fix: hashRouter on dev env break error pages

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
+import "./Error.scss";
 /**
  * 
  * @returns a component that displays the error page
  */
 export function Error() {
     return (
-        <div>
+        <div className="errorCtn">
             <h4>Erreur 404</h4>
-            <p>Une erreur est survenue</p>
+            <p>La page n'existe pas.</p>
+            <Link className="homeLink" to="/">Home</Link>
         </div>
     )
 }
