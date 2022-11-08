1f25be36a376c29ccb34e295ae309dc7263de851
Alex <collo@DESKTOP-0MTRO7E>
2022-11-08 19:02:33 +0100

    feat: error page

--- src/layout/navigations/RouterNavigation.tsx
+++ src/layout/navigations/RouterNavigation.tsx
@@ -4,12 +4,18 @@ import {
     Route,
 } from "react-router-dom";
 import { EmployeeList } from "../../pages/employeeList/EmployeeList";
+import { Error } from "../../pages/error/Error";
 import { Home } from "../../pages/home/Home";
+/**
+ * 
+ * @returns a component that displays the navigation
+ */
 export function RouterNavigation() {
     return (
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="employeeList" element={<EmployeeList/>}/>
+            <Route path="*" element={<Error />}></Route>
         </Routes>
     )
 }
---
+++ src/pages/error/Error.tsx
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @returns a component that displays the error page
+ */
+export function Error() {
+    return (
+        <div>
+            <h4>Erreur 404</h4>
+            <p>Une erreur est survenue</p>
+        </div>
+    )
+}
