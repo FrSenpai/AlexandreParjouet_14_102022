66ad41c11bb8d2978f4d0eacddeda60d34855132
Alex <collo@DESKTOP-0MTRO7E>
2022-11-08 19:40:59 +0100

    fixtures: doc routing hash router

--- src/layout/navigations/RouterNavigation.tsx
+++ src/layout/navigations/RouterNavigation.tsx
@@ -11,11 +11,12 @@ import { Home } from "../../pages/home/Home";
  * @returns a component that displays the navigation
  */
 export function RouterNavigation() {
+    //The error page won't trigger on localhost because hash router is used, it's working on the deployed version
     return (
-        <Routes>
+        <Routes> 
             <Route path="/" element={<Home/>}/>
             <Route path="employeeList" element={<EmployeeList/>}/>
-            <Route path="*" element={<Error />}></Route>
+            <Route path="*" element={<Error />}/>
         </Routes>
     )
 }
